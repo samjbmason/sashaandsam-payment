e509fdfbf506a8115eb854b20b590dc4076afbf2
Sam Mason <sam.jbmason@gmail.com>
2015-07-13 13:39:04 +0100

    Add in cors origin to stop pesky servers hacks

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   before do
    content_type :json
-   headers 'Access-Control-Allow-Origin' => '*',
+   headers 'Access-Control-Allow-Origin' => 'https://sashandsam.co.uk',
            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
            'Access-Control-Allow-Headers' => 'Content-Type'
   end
